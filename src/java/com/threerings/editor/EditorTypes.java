987b75b2f67587607d701e14b6f82f67200ada75
Andrzej Kapolka <andrzej@threerings.net>
2008-06-24 00:11:24 +0000

    Left this out.
    git-svn-id: svn+ssh://src.earth.threerings.net/clyde/trunk@198 edc67d0a-ffea-dc11-983e-003048321218

---
+++ src/java/com/threerings/editor/EditorTypes.java
@@ -0,0 +1,24 @@
+//
+// $Id$
+
+package com.threerings.editor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Specifies the classes available for selection in the editor.  Can be used both on superclasses
+ * and on editable properties.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD, ElementType.METHOD, ElementType.TYPE })
+public @interface EditorTypes
+{
+    /** The subtypes from which to choose. */
+    Class[] value () default {};
+
+    /** The label to use for the type chooser. */
+    String label () default "type";
+}
