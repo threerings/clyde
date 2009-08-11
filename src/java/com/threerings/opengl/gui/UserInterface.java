3ac67fcdb7d548396a5ff9164e02b7c5e91db416
Andrzej Kapolka <andrzej@threerings.net>
2009-08-11 19:55:51 +0000

    Added an accessor for the user interface's (wrapped) context.
    git-svn-id: svn+ssh://src.earth.threerings.net/clyde/trunk@909 edc67d0a-ffea-dc11-983e-003048321218

--- src/java/com/threerings/opengl/gui/UserInterface.java
+++ src/java/com/threerings/opengl/gui/UserInterface.java
@@ -103,6 +103,15 @@ public class UserInterface extends Container
     }
 
     /**
+     * Returns a reference to the interface context, which provides access to the interface
+     * config manager.
+     */
+    public GlContext getContext ()
+    {
+        return _ctx;
+    }
+
+    /**
      * Returns a reference to the interface's scope.
      */
     public DynamicScope getScope ()
