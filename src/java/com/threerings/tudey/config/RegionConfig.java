7809be6ca888d35a21aaee565f0e29cdbe7ec797
Mark Johnson <mjohnson@threerings.net>
2010-05-18 23:30:58 +0000

    - Regions want to be streamed
    git-svn-id: svn+ssh://src.earth.threerings.net/clyde/trunk@1268 edc67d0a-ffea-dc11-983e-003048321218

--- src/java/com/threerings/tudey/config/RegionConfig.java
+++ src/java/com/threerings/tudey/config/RegionConfig.java
@@ -24,6 +24,8 @@
 
 package com.threerings.tudey.config;
 
+import com.threerings.io.Streamable;
+
 import com.threerings.editor.Editable;
 import com.threerings.editor.EditorTypes;
 import com.threerings.editor.Strippable;
@@ -40,7 +42,7 @@ import com.threerings.tudey.shape.config.ShapeConfig;
     RegionConfig.Fixed.class })
 @Strippable
 public abstract class RegionConfig extends DeepObject
-    implements Exportable
+    implements Exportable, Streamable
 {
     /**
      * Base class for the located region configs.
