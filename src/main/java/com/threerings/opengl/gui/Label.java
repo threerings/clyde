ef4a67bc849840d56898c655aa65e5328988e554
Andrzej Kapolka <andrzej@threerings.net>
2011-09-02 23:56:23 +0000

    Comment fix.
    git-svn-id: svn+ssh://src.earth.threerings.net/clyde/trunk@1898 edc67d0a-ffea-dc11-983e-003048321218

--- src/main/java/com/threerings/opengl/gui/Label.java
+++ src/main/java/com/threerings/opengl/gui/Label.java
@@ -57,8 +57,8 @@ public class Label extends TextComponent
     }
 
     /**
-     * Creates a label that will display the supplied text and icon using the specified style
-     * config. All arguments can be null.
+     * Creates a label that will display the supplied text and icon (either of which can be
+     * null).
      */
     public Label (GlContext ctx, Icon icon, String text)
     {
