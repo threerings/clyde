9ea0e8535205d764b2e901511a39f40722981c31
Andrzej Kapolka <andrzej@threerings.net>
2011-11-11 02:09:38 +0000

    Starting on support for "interface scripts" which will provide a means of defining simple animations and such (mostly intended for tutorials).
    git-svn-id: svn+ssh://src.earth.threerings.net/clyde/trunk@1945 edc67d0a-ffea-dc11-983e-003048321218

---
+++ src/main/java/com/threerings/opengl/gui/config/InterfaceScriptConfig.java
@@ -0,0 +1,69 @@
+//
+// $Id$
+//
+// Clyde library - tools for developing networked games
+// Copyright (C) 2005-2011 Three Rings Design, Inc.
+// http://code.google.com/p/clyde/
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+//    conditions and the following disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package com.threerings.opengl.gui.config;
+
+import com.threerings.config.ConfigReference;
+import com.threerings.config.ParameterizedConfig;
+import com.threerings.editor.Editable;
+import com.threerings.editor.EditorTypes;
+import com.threerings.export.Exportable;
+import com.threerings.util.DeepObject;
+
+/**
+ * A script used to control a user interface.
+ */
+public class InterfaceScriptConfig extends ParameterizedConfig
+{
+    /**
+     * Contains the actual implementation of the script.
+     */
+    @EditorTypes({ Original.class, Derived.class })
+    public static abstract class Implementation extends DeepObject
+        implements Exportable
+    {
+    }
+
+    /**
+     * An original implementation.
+     */
+    public static class Original extends Implementation
+    {
+    }
+
+    /**
+     * A derived implementation.
+     */
+    public static class Derived extends Implementation
+    {
+        /** The script reference. */
+        @Editable(nullable=true)
+        public ConfigReference<InterfaceScriptConfig> interfaceScript;
+    }
+
+    /** The actual script implementation. */
+    @Editable
+    public Implementation implementation = new Original();
+}
