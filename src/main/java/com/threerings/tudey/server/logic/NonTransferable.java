2763d934d12ea8d84ad9b14243b8927be68f4977
Mark Johnson <mjohnson@threerings.net>
2013-07-05 13:28:20 -0700

    - Added a NonTransferable interface for Logics that should not be transfered by the scene manager

---
+++ src/main/java/com/threerings/tudey/server/logic/NonTransferable.java
@@ -0,0 +1,11 @@
+//
+// $Id$
+
+package com.threerings.tudey.server.logic;
+
+/**
+ * An interface to mark logics that should not be transfered by the scene manager.
+ */
+public interface NonTransferable
+{
+}
