1ed8297af757607048c3cfb3b296e68c7a66df36
Ray J. Greenwell <ray@threerings.net>
2013-07-15 22:48:59 -0700

    Added find() and improved field mapping.

--- src/main/java/com/threerings/config/tools/ConfigSearcher.java
+++ src/main/java/com/threerings/config/tools/ConfigSearcher.java
@@ -5,11 +5,13 @@ package com.threerings.config.tools;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 import java.util.List;
 import java.util.Set;
 
 import com.google.common.base.Function;
+import com.google.common.base.Predicate;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
@@ -36,6 +38,66 @@ public class ConfigSearcher
     }
 
     /**
+     * Find if anything satisfies the predicate in the specified object any any sub-objects.
+     */
+    public static boolean find (Object val, Predicate<? super ConfigReference<?>> detector)
+    {
+        return find(val, detector, Sets.newIdentityHashSet());
+    }
+
+    /**
+     * Internal helper for find.
+     */
+    protected static boolean find (
+        Object val, Predicate<? super ConfigReference<?>> detector,
+        Set<Object> seen)
+    {
+        if (val == null) {
+            return false;
+        }
+
+        Class<?> c = val.getClass();
+        if ((c == String.class) || c.isPrimitive() || Primitives.isWrapperType(c) ||
+                !seen.add(val)) {
+            return false;
+        }
+
+        // make a list of sub-fields
+        if (c.isArray()) {
+            for (int ii = 0, nn = Array.getLength(val); ii < nn; ii++) {
+                if (find(Array.get(val, ii), detector, seen)) {
+                    return true;
+                }
+            }
+
+        } else if (val instanceof ConfigReference) {
+            ConfigReference<?> ref = (ConfigReference<?>)val;
+            if (detector.apply(ref)) {
+                return true;
+            }
+            for (Object value : ref.getArguments().values()) {
+                if (find(value, detector, seen)) {
+                    return true;
+                }
+            }
+
+        } else {
+            for (Field f : FIELDS.getUnchecked(c)) {
+                Object o;
+                try {
+                    o = f.get(val);
+                } catch (IllegalAccessException iae) {
+                    continue;
+                }
+                if (find(o, detector, seen)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
      * Internal helper for findAttributes.
      */
     protected static <T> Iterable<T> findAttributes (
@@ -54,16 +116,16 @@ public class ConfigSearcher
 
         // make a list of sub-fields
         List<Iterable<T>> list = Lists.newArrayList();
-        if (c.isArray()) {
-            for (int ii = 0, nn = Array.getLength(val); ii < nn; ii++) {
-                list.add(findAttributes(Array.get(val, ii), detector, seen));
-            }
-
-        } else if (val instanceof ConfigReference) {
+        if (val instanceof ConfigReference) {
             ConfigReference<?> ref = (ConfigReference<?>)val;
             list.add(detector.apply(ref));
             for (Object value : ref.getArguments().values()) {
                 list.add(findAttributes(value, detector, seen));
+            }
+
+        } else if (c.isArray()) {
+            for (int ii = 0, nn = Array.getLength(val); ii < nn; ii++) {
+                list.add(findAttributes(Array.get(val, ii), detector, seen));
             }
 
         } else {
@@ -80,18 +142,28 @@ public class ConfigSearcher
         return Iterables.concat(list);
     }
 
-    /** A cache of Class -> Field[], since calling getFields() copies each time to protect the
-     * original array. */
-    protected static final LoadingCache<Class<?>, Field[]> FIELDS = CacheBuilder.newBuilder()
+    /** All the fields (and superfields...) of a class, cached. */
+    protected static final LoadingCache<Class<?>, ImmutableList<Field>> FIELDS =
+        CacheBuilder.newBuilder()
         .concurrencyLevel(1)
         .build(
-            new CacheLoader<Class<?>, Field[]>() {
-                public Field[] load (Class<?> c) {
-                    Field[] fa = c.getFields();
-                    for (Field f : fa) {
-                        f.setAccessible(true);
+            new CacheLoader<Class<?>, ImmutableList<Field>>() {
+                public ImmutableList<Field> load (Class<?> clazz) {
+                    ImmutableList.Builder<Field> builder = ImmutableList.builder();
+                    // add recurse on superclass
+                    Class<?> superClazz = clazz.getSuperclass();
+                    if (superClazz != null) {
+                        builder.addAll(FIELDS.getUnchecked(superClazz));
                     }
-                    return fa;
+                    // get all fields of the specified class, and filter out the static ones..
+                    for (Field f : clazz.getDeclaredFields()) {
+                        // add all non-static fields; make them accessible
+                        if (0 == (f.getModifiers() & Modifier.STATIC)) {
+                            f.setAccessible(true);
+                            builder.add(f);
+                        }
+                    }
+                    return builder.build();
                 }
             });
 }
