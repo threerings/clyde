b0a79aa6dd5e86f91c958d94e39643a8b60df0d0
Ray J. Greenwell <ray@threerings.net>
2015-01-05 13:52:30 -0800

    Added a note about package annotations.
    They are fundamentally flawed. Packages are only resolved when
    a class within them is, and there's no way to force it to occur,
    as far as I can tell.
    
    So the results of looking for package annotations depend on what
    other code has been running before that. Fucked up.

--- core/src/main/java/com/threerings/editor/Introspector.java
+++ core/src/main/java/com/threerings/editor/Introspector.java
@@ -120,6 +120,18 @@ public class Introspector
 
     /**
      * Finds the editor message bundle for the supplied class.
+     *
+     * <P><em>NOTE</em>: if the class itself doesn't have an EditorMessageBundle annotation,
+     * this code attempts to find the annotation on the package, climbing up the package
+     * hierarchy one level at a time.
+     *
+     * <P>However, this is fundamentally flawed. Packages are not resolved until a class
+     * within them is, nor is there a way to force the resolution of a package. So the results
+     * of this method will change over the lifetime of the JVM depending on what other
+     * code has been running.
+     *
+     * <P>That is a shit situation. My recommendation is to create a package-info.java
+     * directly in the desired package, and duplicate the annotation as necessary.
      */
     protected static String findMessageBundle (Class<?> clazz)
     {
