9a875ab69ee073de60bbded26b657ab0c2d27065
Ray J. Greenwell <ray@threerings.net>
2018-11-15 12:08:14 -0800

    Restore normalcy.

--- core/src/main/java/com/threerings/config/ParameterizedConfig.java
+++ core/src/main/java/com/threerings/config/ParameterizedConfig.java
@@ -101,7 +101,7 @@ public class ParameterizedConfig extends ManagedConfig
         if (_derived == null) {
             _derived = CacheUtil.softValues(1);
         }
-        ParameterizedConfig instance = null; //_derived.get(filteredArgs);
+        ParameterizedConfig instance = _derived.get(filteredArgs);
         if (instance == null) {
             if (derivedArgs == null) {
                 derivedArgs = filteredArgs.clone();
