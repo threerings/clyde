baeb8e6b65f3c39dd9a3ab5989a4f77d63084b30
Ray J. Greenwell <ray@bogocorp.com>
2025-01-22 11:13:44 -0800

    Revert "TEMP Disable caching on these two configs to support px color editor."
    This reverts commit c72b6f5583f05f45908f3c3b70b2f6fca32c333d.

--- core/src/main/java/com/threerings/config/ParameterizedConfig.java
+++ core/src/main/java/com/threerings/config/ParameterizedConfig.java
@@ -101,8 +101,7 @@ public class ParameterizedConfig extends ManagedConfig
         if (_derived == null) {
             _derived = CacheUtil.softValues(1);
         }
-        //ParameterizedConfig instance = _derived.get(filteredArgs);
-        ParameterizedConfig instance = null;
+        ParameterizedConfig instance = _derived.get(filteredArgs);
         if (instance == null) {
             if (derivedArgs == null) {
                 derivedArgs = filteredArgs.clone();
